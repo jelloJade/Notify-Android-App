169a3778e2a88e96b75bac840b06fb801b93eb56
debbarmaonit <debbarmaonit@gmail.com>
2016-03-07 13:22:08 +0530

    Finished App Commit

---
+++ app/src/main/java/com/example/shibajyotidebbarma/notify/DateTimeSorter.java
@@ -0,0 +1,34 @@
+package com.example.shibajyotidebbarma.notify;
+
+/**
+ * Created by shibajyotidebbarma on 07/03/16.
+ */
+public class DateTimeSorter {
+    public int mIndex;
+    public String mDateTime;
+
+
+    public DateTimeSorter(int index, String DateTime){
+        mIndex = index;
+        mDateTime = DateTime;
+    }
+
+    public DateTimeSorter(){}
+
+
+    public int getIndex() {
+        return mIndex;
+    }
+
+    public void setIndex(int index) {
+        mIndex = index;
+    }
+
+    public String getDateTime() {
+        return mDateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        mDateTime = dateTime;
+    }
+}
